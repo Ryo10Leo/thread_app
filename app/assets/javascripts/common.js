c79b3240817a193ede08a67fd510be8f92e77426
Ryo10Leo <ryo.bbl9@gmail.com>
2020-02-24 01:08:21 +0900

    ページトップボタン追加

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,23 @@
+$(document).on('turbolinks:load', function(){
+  //page topボタン
+
+  $(function(){
+    var topBtn=$('#pageTop');
+    topBtn.hide();
+  
+    $(window).scroll(function(){
+      if($(this).scrollTop()>80　&& $(window).width() > 559){
+        topBtn.fadeIn();
+      }else{
+        topBtn.fadeOut();
+      }
+    });
+  
+    topBtn.click(function(){
+      $('body,html').animate({
+      scrollTop: 0},500);
+      return false;
+    });
+  
+    });
+});
